4d2b0714e896bd2fb1ffaa2eacdeab9cfc84e196
jakebryon <jakebryon1995@berkeley.edu>
2017-05-23 21:51:34 -0700

    Planet with Rocket
    set up for rocket pointing radially out from planet at the starting
    position.

--- game.js
+++ game.js
@@ -1,18 +1,35 @@
-var renderer = PIXI.autoDetectRenderer(1000, 500);
+canvasHeight = 500;
+canvasWidth = 1000;
+var renderer = PIXI.autoDetectRenderer(canvasWidth, canvasHeight);
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
-PIXI.loader.add("bunny.png").load(setup);
+PIXI.loader.add("planet.png").add("rocket.png").load(setup);
 
-var sprite;
+var planet;
+var rocket;
 function setup() {
-	sprite = new PIXI.Sprite(PIXI.loader.resources["bunny.png"].texture);
-	stage.addChild(sprite);
+	planet = new PIXI.Sprite(PIXI.loader.resources["planet.png"].texture);
+	planet.scale.x = .2;
+	planet.scale.y = .2;
+	planet.x = canvasWidth/2 - planet.width/2;
+	planet.y = canvasHeight/2 - planet.height/2;
+	
+	rocket = new PIXI.Sprite(PIXI.loader.resources["rocket.png"].texture);
+	rocket.scale.x = .2;
+	rocket.scale.y = .2;
+	rocket.rotation = 1.5707;
+	rocket.x = canvasWidth/2 + rocket.height/2 + planet.width/2;
+	rocket.y = canvasHeight/2 - rocket.width/2;
+	/*rocket.anchor.x = .5;
+	rocket.anchor.y = .5;*/
+	
+	stage.addChild(planet);
+	stage.addChild(rocket);
 }
 
 function gameLoop() {
 	requestAnimationFrame(gameLoop);
-	sprite.rotation += 0.1;
 	renderer.render(stage);
 }
 
